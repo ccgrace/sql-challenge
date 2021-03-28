75556d79941581a0f8f86a182b17a721a5976712
ccgrace <77289566+ccgrace@users.noreply.github.com>
2021-03-28 12:27:52 -0400

    added queries file.

---
+++ Queries.sql
@@ -0,0 +1,12 @@
+--List the following details of each employee: employee number, last name, first name, sex, and salary.
+SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
+FROM employees
+JOIN salaries
+ON employees.emp_no = salaries.emp_no
+
+--List first name, last name, and hire date for employees who were hired in 1986.
+SELECT employees.first_name, employees.last_name, employees.hire_date
+FROM employees
+WHERE employees.hire_date LIKE '%1986'
+
+--
