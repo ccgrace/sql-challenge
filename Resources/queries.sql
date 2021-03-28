30356598f0c32036d7f0cbda87b8420bea720627
ccgrace <77289566+ccgrace@users.noreply.github.com>
2021-03-28 12:57:51 -0400

    completed query 7.

--- Resources/queries.sql
+++ Resources/queries.sql
@@ -41,4 +41,13 @@ JOIN departments
 ON dept_emp.dept_no = departments.dept_no
 WHERE departments.dept_name = 'Sales'
 
+--List all employees in the Sales and Development departments, 
+--including their employee number, last name, first name, and department name.
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM employees
+JOIN dept_emp
+ON employees.emp_no = dept_emp.emp_no
+JOIN departments
+ON dept_emp.dept_no = departments.dept_no
+WHERE departments.dept_name = 'Sales' OR departments.dept_name = 'Development'
 
