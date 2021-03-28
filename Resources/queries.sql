7d688caed131963b3f22243edadf8fa205f2edf3
ccgrace <77289566+ccgrace@users.noreply.github.com>
2021-03-28 12:48:35 -0400

    completes query 5.

--- Resources/queries.sql
+++ Resources/queries.sql
@@ -2,12 +2,12 @@
 SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
 FROM employees
 JOIN salaries
-ON employees.emp_no = salaries.emp_no
+ON employees.emp_no = salaries.emp_no;
 
 --List first name, last name, and hire date for employees who were hired in 1986.
 SELECT employees.first_name, employees.last_name, employees.hire_date
 FROM employees
-WHERE employees.hire_date LIKE '%1986'
+WHERE employees.hire_date LIKE '%1986';
 
 --List the manager of each department with the following information: 
 --department number, department name, the manager's employee number, last name, first name.
@@ -16,7 +16,7 @@ FROM departments
 JOIN dept_manager
 ON departments.dept_no = dept_manager.dept_no
 JOIN employees
-ON dept_manager.emp_no = employees.emp_no
+ON dept_manager.emp_no = employees.emp_no;
 
 --List the department of each employee with the following information: 
 --employee number, last name, first name, and department name.
@@ -25,4 +25,10 @@ FROM departments
 JOIN dept_emp
 ON departments.dept_no = dept_emp.dept_no
 JOIN employees
-ON dept_emp.emp_no = employees.emp_no
+ON dept_emp.emp_no = employees.emp_no;
+
+--List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+SELECT employees.first_name, employees.last_name, employees.sex
+FROM employees
+WHERE employees.first_name = 'Hercules' AND employees.last_name LIKE 'B%';
+
