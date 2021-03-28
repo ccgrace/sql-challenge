168e32d1279f6f7e8e73b16d8c8219c4ab1cd489
ccgrace <77289566+ccgrace@users.noreply.github.com>
2021-03-28 12:44:24 -0400

    completed query 4.

---
+++ Resources/queries.sql
@@ -0,0 +1,28 @@
+--List the following details of each employee: employee number, last name, first name, sex, and salary.
+SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
+FROM employees
+JOIN salaries
+ON employees.emp_no = salaries.emp_no
+
+--List first name, last name, and hire date for employees who were hired in 1986.
+SELECT employees.first_name, employees.last_name, employees.hire_date
+FROM employees
+WHERE employees.hire_date LIKE '%1986'
+
+--List the manager of each department with the following information: 
+--department number, department name, the manager's employee number, last name, first name.
+SELECT departments.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name
+FROM departments
+JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no
+JOIN employees
+ON dept_manager.emp_no = employees.emp_no
+
+--List the department of each employee with the following information: 
+--employee number, last name, first name, and department name.
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM departments
+JOIN dept_emp
+ON departments.dept_no = dept_emp.dept_no
+JOIN employees
+ON dept_emp.emp_no = employees.emp_no
