839d052d62db744d07ccc79b19b02c12d2e7e8a7
ccgrace <77289566+ccgrace@users.noreply.github.com>
2021-03-28 11:39:47 -0400

    adjusted spacing

--- EmployeeSQL.sql
+++ EmployeeSQL.sql
@@ -36,13 +36,18 @@ CREATE TABLE "salaries" (
 
 ALTER TABLE "dept_emp" ADD 
 	FOREIGN KEY ("dept_no") REFERENCES "departments"("dept_no");
+	
 ALTER TABLE "dept_emp" ADD 
 	FOREIGN KEY ("emp_no") REFERENCES "employees"("emp_no");
+	
 ALTER TABLE "dept_manager" ADD
 	FOREIGN KEY ("dept_no") REFERENCES "departments"("dept_no");
+	
 ALTER TABLE "dept_manager" ADD
 	FOREIGN KEY ("emp_no") REFERENCES "employees"("emp_no");
+	
 ALTER TABLE "employees" ADD
 	FOREIGN KEY ("emp_title_id") REFERENCES "titles"("title_id");
+	
 ALTER TABLE "salaries" ADD
 	FOREIGN KEY ("emp_no") REFERENCES "employees"("emp_no");
